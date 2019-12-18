b6e3174fa286955985263672a3bb5a6d697baafc
sasha-murray <sasha.murray@appliedtechnology.se>
2019-12-18 15:23:25 +0100

    initial react setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Landing from './pages/Landing/Landing'
+import Chat from './pages/Chat/Chat'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+
+        <Route path="/chat">
+          <Chat/>
+        </Route>
+
+
+        <Route path="/">
+          <Landing/>
+        </Route>
+
+      </Switch>
+    </Router>
+
+  )
 }
 
-export default App;
+export default App
